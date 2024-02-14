926ea120da586b2d667d330463df7bd67ef633d2
Nolwenn <np.cousin@gmail.com>
2024-02-14 12:19:57 +0100

    ajout test endpoint /secure-data/

--- API_test.py
+++ API_test.py
@@ -4,9 +4,12 @@ import pytest
 
 client = TestClient(api)
 
-
+# Test du endpoint GET /test
 def test_get_test():
     assert client.get("/test").status_code == 200
     assert client.get("/test").json() == {'message': 'API is functional'}
 
-
+# Test du endpoint GET /secure-data/
+def test_get_secure_data():
+    assert client.get("/secure-data/", headers={"Authorization": "Basic YWRtaWGRtMU4="}).status_code == 401
+    assert client.get("/secure-data/", headers={"Authorization": "Basic YWRtaW46ZGF0YXNjaWVudGVzdAo="}).status_code == 200
