a28c36951d1d384ff46f222f572b865f64150f5e
SylvainMazoyer <sylvainmazoyerperso@gmail.com>
2024-02-15 09:27:08 +0100

    tentative d'utilisation de la base IMDB 1000 dans un nb

--- src/models/train_CBF_model.py
+++ src/models/train_CBF_model.py
@@ -1,10 +1,10 @@
 import pandas as pd
+import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-from tabulate import tabulate
 
-# Import du jeu de données sur les films
-df = pd.read_csv('../data/movies.csv')
+# Import du jeu de données sur les films dans le répertoire data à la racine du projet
+df = pd.read_csv('./data/movies.csv')
 
 # formattage de la colonne genre
 df['Genres'] = df['genres'].apply(lambda x: ' '.join(x.split('|')))
@@ -14,3 +14,17 @@ df['Title'] = df["title"].apply(lambda x:x[:-6])
 
 # création de la colonne descriptive de l'ensemble du film
 df['Description'] = df['Title'] + df['Genres']
+
+# Créer un TfidfVectorizer et supprimer les mots vides
+tfidf = TfidfVectorizer(stop_words='english')
+
+# Adapter et transformer les données en une matrice tfidf
+matrice_tfidf = tfidf.fit_transform(df['Description'])
+
+# On calcule la similarité cosinus
+sim_cosinus = cosine_similarity(matrice_tfidf, matrice_tfidf)
+
+# Créer une série d'indices en utilisant la colonne 'title' comme index
+indices = pd.Series(range(0,len(df)), index=df['title'])
+
+
