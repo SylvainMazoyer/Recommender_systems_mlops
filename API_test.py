c82e4dc7e4010d4c0d1213fb1858014f60aa552a
MadigRG <153445931+MadigRG@users.noreply.github.com>
2024-02-20 20:41:00 +0100

    Update API_test.py

--- API_test.py
+++ API_test.py
@@ -6,10 +6,10 @@ client = TestClient(api)
 
 # Test du endpoint GET /test
 def test_get_test():
-    assert client.get("/test").status_code == 200
-    assert client.get("/test").json() == {'message': 'API is functional'}
+    assert client.get("/").status_code == 200
+    assert client.get("/").json() == {'message': 'API is functional'}
 
 # Test du endpoint GET /secure-data/
 def test_get_secure_data():
-    assert client.get("/secure-data/", headers={"Authorization": "Basic YWRtaWGRtMU4="}).status_code == 401
-    assert client.get("/secure-data/", headers={"Authorization": "Basic YWRtaW46ZGF0YXNjaWVudGVzdA=="}).status_code == 200
+    assert client.get("/admin", headers={"Authorization": "Basic YWRtaWGRtMU4="}).status_code == 401
+    assert client.get("/admin", headers={"Authorization": "Basic YWRtaW46ZGF0YXNjaWVudGVzdA=="}).status_code == 200
