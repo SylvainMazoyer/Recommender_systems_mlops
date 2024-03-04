b63f1916346ff057e65aa299b806384e56ddee0a
SylvainMazoyer <sylvainmazoyerperso@gmail.com>
2024-03-04 16:26:23 +0100

    script d'entrainement du modèle et enregsitrement de la matrice dans un fichier, sous forme de function

--- src/models/train_CBF_model.py
+++ src/models/train_CBF_model.py
@@ -3,28 +3,30 @@ import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-# Import du jeu de données sur les films dans le répertoire data à la racine du projet
-df = pd.read_csv('./data/films.csv')
 
-# formattage de la colonne genre
-df['Genres'] = df['genres'].apply(lambda x: ' '.join(x.split('|')))
+def train_CBF_model():
+    # Import du jeu de données sur les films dans le répertoire data à la racine du projet
+    df = pd.read_csv('./data/films.csv')
 
-# création de la colonne descriptive de l'ensemble du film
-df['Description'] = df['title'] + " " + df['Genres']
+    # formattage de la colonne genre
+    df['Genres'] = df['genres'].apply(lambda x: ' '.join(x.split('|')))
 
-# Créer un TfidfVectorizer et supprimer les mots vides
-tfidf = TfidfVectorizer(stop_words='english')
+    # création de la colonne descriptive de l'ensemble du film
+    df['Description'] = df['title'] + " " + df['Genres']
 
-# Adapter et transformer les données en une matrice tfidf
-matrice_tfidf = tfidf.fit_transform(df['Description'])
+    # Créer un TfidfVectorizer et supprimer les mots vides
+    tfidf = TfidfVectorizer(stop_words='english')
 
-# On calcule la similarité cosinus
-sim_cosinus = cosine_similarity(matrice_tfidf, matrice_tfidf)
+    # Adapter et transformer les données en une matrice tfidf
+    matrice_tfidf = tfidf.fit_transform(df['Description'])
 
-# conversion du type de la similarité cosinus en float16 pour améliorer le temps de chargement de cette matrice pour faire les prédictions
-sim_cosinus = sim_cosinus.astype('float16')
+    # On calcule la similarité cosinus
+    sim_cosinus = cosine_similarity(matrice_tfidf, matrice_tfidf)
 
-# enregistrement dans un fichier
-np.savetxt("./data/sim_cos_CBF", sim_cosinus)
+    # conversion du type de la similarité cosinus en float16 pour améliorer le temps de chargement de cette matrice pour faire les prédictions
+    sim_cosinus = sim_cosinus.astype('float16')
+
+    # enregistrement dans un fichier
+    np.savetxt("./data/sim_cos_CBF", sim_cosinus)
 
 
