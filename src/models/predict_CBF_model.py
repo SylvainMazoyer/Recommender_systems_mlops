514cfcf7acedd66252426906e207a90a5c360263
SylvainMazoyer <sylvainmazoyerperso@gmail.com>
2024-03-15 16:37:25 +0100

    transformation des résultats de reco en df, testé et approuvé

--- src/models/predict_CBF_model.py
+++ src/models/predict_CBF_model.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import numpy as np
 
-def recommandations_CBF(df, titre, mat_sim, num_recommendations = 10):
+def recommandations_CBF(df_films, titre, mat_sim, num_recommendations = 10):
 
         #  Créer une série d'indices en utilisant la colonne 'title' comme index
-    indices = pd.Series(range(0,len(df)), index=df['title'])
+    indices = pd.Series(range(0,len(df_films)), index=df_films['title'])
 
 
         # On récupère l'indice associé au titre qui servira à identifier le livre dans la matrice de similarité
@@ -25,11 +25,31 @@ def recommandations_CBF(df, titre, mat_sim, num_recommendations = 10):
         # Renvoyer les titres des films les plus similaires
     recommended_movies = [indices.index[idx] for idx, score in top_similair]
 
-    reco = {}
+    #print(recommended_movies)
 
-    for i in range(0, 5):
+    results = df_films[df_films["title"] == recommended_movies[0]]
 
-        movieId = df[df["title"] == recommended_movies[i]]["movieId"].iloc[0]
-        reco[movieId] = recommended_movies[i]
+    for title in recommended_movies[1: len(recommended_movies)]:
 
-    return reco
+        df_tmp = df_films[df_films["title"] == title]
+        results = pd.concat([results, df_tmp], ignore_index=True)
+
+    #results.reset_index(inplace=True)
+
+    return results
+
+"""from src.models.load_CBF_similarity_matrix import load_CBF_similarity_matrix
+mat_sim = load_CBF_similarity_matrix()
+films_path = './data/films.csv'
+df_films = pd.read_csv(films_path)
+title = df_films[df_films["movieId"] == 1]["title"].iloc[0]
+
+reco = recommandations_CBF(df_films, title, mat_sim, num_recommendations = 5)
+
+print(reco)
+
+import json
+results_json = reco.to_json(orient="records")
+
+r = json.loads(results_json)
+print(r)"""
