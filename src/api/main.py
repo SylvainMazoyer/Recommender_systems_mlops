040431717a0cabbea0784a8dc00f78dea3a9685f
Nolwenn <np.cousin@gmail.com>
2024-03-16 11:09:35 +0100

    Finalisation conversion endpoints csv vers  postgre

--- src/api/main.py
+++ src/api/main.py
@@ -8,6 +8,11 @@ import psycopg2
 from typing import Optional
 from passlib.context import CryptContext
 import time
+import logging 
+
+# Configuration du logging
+logging.basicConfig(filename='logs/API_log.log', encoding='utf-8', level=logging.INFO,
+                    format='%(asctime)s : %(message)s', datefmt='%m/%d/%Y %H:%M:%S')
 
 
 api = FastAPI(
@@ -64,27 +69,29 @@ def verify_admin(credentials: HTTPBasicCredentials = Depends(security)):
     
         # Si le mot de passe saisi ne correspond pas à celui de la base, on lève une erreur 
         if not (pwd_context.verify(credentials.password, db_password)):
+            logging.info('%s : ERREUR 401 : Accès admin non autorisé', credentials.username)
             raise HTTPException(
                                 status_code=status.HTTP_401_UNAUTHORIZED,
-                                detail="Incorrect email or password",
+                                detail="Mot de passe incorrect",
                                 headers={"WWW-Authenticate": "Basic"},
                             )
 
     # si le username n'est pas présent dans la table admin, on lève une erreur       
     else:
+        logging.info('%s : ERREUR 401 : Accès admin non autorisé', credentials.username)
         raise HTTPException(
                                 status_code=status.HTTP_401_UNAUTHORIZED,
-                                detail="Incorrect email or password",
+                                detail="Utilisateur inconnu",
                                 headers={"WWW-Authenticate": "Basic"},
                             )
 
     conn.close()
-    return credentials.username
+    return credentials.username, db_role
 
 
 
-@api.get("/admin")
-async def get_secure_data(username: str = Depends(verify_admin)):
+@api.get("/admin/{asked_role}")
+async def get_secure_data(asked_role, user_rights: tuple = Depends(verify_admin)):
     """
     Description:
     Cette route renvoie un message de bienvenue personnalisé en utilisant le nom d'utilisateur fourni en tant que dépendance.
@@ -100,7 +107,16 @@ async def get_secure_data(username: str = Depends(verify_admin)):
     `get_secure_data` échoue pour récupérer le nom d'utilisateur. 
     Dans ce cas, une exception FastAPI sera levée automatiquement.
     """
-    #logging.info('%s : Accès admin autorisé', username)
+    username, role = user_rights
+
+    if asked_role == 'Data' and role!='Data':
+        logging.info('%s : ERREUR 401 : Accès admin non autorisé', username)
+        raise HTTPException(
+                        status_code=status.HTTP_401_UNAUTHORIZED,
+                        detail="Droits insuffisants",
+                        headers={"WWW-Authenticate": "Basic"},
+                    )
+    logging.info('%s : Accès admin autorisé', username)
     return {"message": f"Hello {username}, you have access to secure data"}
 
 
@@ -159,11 +175,14 @@ def create_user(user_data: CreateUser):
 
         
         response = {"message": "user created successfully", "userId": rows[0]}
+        logging.info('%s : Accès API POST /create-user : user créé', rows[0])
+
         cur.close()
 
     # Si une ligne remonte, on ne fait rien
     else:
         response = {"message": "user already exists", "userId": rows[0]}
+        logging.info('%s : Accès API POST /create-user : user déjà existant', rows[0])
     
     conn.close()
     return response
@@ -184,6 +203,7 @@ async def pred_rand_model():
 
     """    
     results = random_recos()
+    logging.info('Accès API GET /predict/rand_model : %s')
     results_json = results.to_json(orient="records")
     return results_json
 
@@ -251,7 +271,7 @@ class CreateMovie(BaseModel):
     youtubeId : Optional[str] = ''
 
 @api.post("/create-movie")
-def create_movie(movie_data: CreateMovie, user: str = Depends(verify_admin)):
+def create_movie(movie_data: CreateMovie, user_rights: tuple = Depends(verify_admin)):
     """
     Crée un nouveau film dans le système.
 
@@ -268,6 +288,7 @@ def create_movie(movie_data: CreateMovie, user: str = Depends(verify_admin)):
         une exception HTTP 401 Unauthorized est levée.
     """
 
+    username, role = user_rights
     new_movie = movie_data.model_dump()
 
     # Connexion à la base postgre
@@ -291,6 +312,8 @@ def create_movie(movie_data: CreateMovie, user: str = Depends(verify_admin)):
         conn.commit()
         cur.close()
         response = {"message": "Film ajoute"}
+        logging.info('%s : Accès API POST /create-movie : film "%s" créé', username, new_movie["title"] )
+
 
     else:
         cur = conn.cursor()
@@ -300,6 +323,7 @@ def create_movie(movie_data: CreateMovie, user: str = Depends(verify_admin)):
         cur.close()
         conn.close()
         response = {"message": "Film mis a jour"}
+        logging.info('%s : Accès API POST /create-movie : film "%s" mis à jour', username, new_movie["title"] )
 
     return response
 
--- streamlit/app.py
+++ streamlit/app.py
@@ -1,11 +1,12 @@
 import streamlit as st
 import requests
 import base64
+from requests.auth import HTTPBasicAuth
 
-def api_call_admin(username,password):
+def api_call_admin(username,password,role):
     str_to_encode = username + ':' + password
     encoded_str = "Basic " + base64.b64encode(str_to_encode.encode()).decode()
-    response = requests.get("http://api_model_container:5000/admin", 
+    response = requests.get(f'http://api_model_container:5000/admin/{role}', 
                         headers={"Authorization": encoded_str})
     st.write(response.content)
 
@@ -14,19 +15,24 @@ def run():
     response = requests.get("http://api_model_container:5000/").json()["message"]
     st.write("Test appel API : ",response)
     st.write(requests.get("http://api_model_container:5000/predict/rand_model").json())
-    response2 = requests.post("http://api_model_container:5000/create-user", json={'name': 'Jean-Charles'}).json()
+    response2 = requests.post("http://api_model_container:5000/create-user", json={'name': 'Charles'}).json()
     st.write("Test appel API create user : ", response2['message'], response2['userId'])
     response3 = requests.post("http://api_model_container:5000/user_activity", json={'userId': '1', 'movieId':9999900})
     st.write(response3.content)
-    response4 = requests.post("http://api_model_container:5000/create-movie", json={'title': 'High School Musical 2', 'genres':'Comedie musicale'})
+    response3bis = requests.post("http://api_model_container:5000/user_activity", json={'userId': '1', 'movieId':5})
+    st.write(response3bis.content)
+    response4 = requests.post("http://api_model_container:5000/create-movie", json={'title': 'Lalalala', 'genres':'Comedy'},auth=("Nolwenn", "54321"))
     st.write(response4.content)
+    response4bis = requests.post("http://api_model_container:5000/create-movie", json={'title': 'Toy Story (1995)', 'genres':'Comedy'},auth=("Nolwenn","54321"))
+    st.write(response4bis.content)
 
 
 
     form = st.form(key='my-form')
     username = form.text_input('Enter your name')
     password = form.text_input('Enter your password')
-    submit = form.form_submit_button('Submit', on_click=api_call_admin, args=(username,password))
+    role = form.text_input('Enter your role')
+    submit = form.form_submit_button('Submit', on_click=api_call_admin, args=(username,password,role))
 
 
 
